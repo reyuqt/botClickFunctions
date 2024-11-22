149bb0d3e560d1e9ee39f077c59237ef09b67dfe
lauren <reyuqt01@gmail.com>
2024-11-22 18:13:03 -0500

    ``` fix: update click_this_element function parameters in tests (main) ```

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,32 +8,50 @@ import os
 
 from click_helper.box import Box
 from click_helper.utils import click_image, click_at_coordinates, locate_image
+from click_helper.selenium_click import save_element
 
 
 @pytest.fixture(scope="module")
 def driver():
     driver = webdriver.Chrome()
     driver.get(f"file://{os.getcwd()}/test.html")
+    driver.maximize_window()
+    save_element(driver.find_element(By.ID, 'test-button-1'), name='test_button')
+    save_element(driver.find_element(By.ID, 'test-input'), name='test_input')
     yield driver
     driver.quit()
 
 
 @pytest.fixture
-def image_box() -> Box:
+def button_box() -> Box:
     return locate_image('test_button.png', region=None, confidence=0.8, min_confidence=0.5, retries=3)
 
-def test_locate_image(driver, image_box):
-    driver_location = driver.find_element(By.ID, 'test-button-1')
+
+@pytest.fixture
+def input_box() -> Box:
+    return locate_image('test_input.png', region=None, confidence=0.8, min_confidence=0.5, retries=3)
 
 
-def test_click_at_coordinates(image_box):
-    x, y = image_box.center()
+def test_locate_image(driver, button_box, input_box):
+    driver_location = driver.find_element(By.ID, 'test-button-1').size
+    assert driver_location['height'] == button_box.height
+    assert driver_location['width'] == button_box.width
+
+    driver_location = driver.find_element(By.ID, 'test-input').size
+    assert driver_location['height'] == input_box.height
+    assert driver_location['width'] == input_box.width
+
+
+def test_click_at_coordinates(driver, button_box, input_box):
+    x, y = button_box.center()
     click_at_coordinates(x=x, y=y, duration_range=(1, 5))
-    time.sleep(1)
     last_clicked = driver.execute_script("return window.lastClickedElement;")
-    time.sleep(1)
     assert last_clicked == 'test-button-1'
 
+    x, y = input_box.center()
+    click_at_coordinates(x=x, y=y, duration_range=(1, 5))
+    last_clicked = driver.execute_script("return window.lastClickedElement;")
+    assert last_clicked == 'test-input'
 
 
 def test_click_image(driver):
@@ -45,4 +63,18 @@ def test_click_image(driver):
                           x_reduction=0.2,
                           y_reduction=0.2,
                           duration_range=(1, 5))
+    last_clicked = driver.execute_script("return window.lastClickedElement;")
     assert clicked
+    assert last_clicked == 'test-button-1'
+
+    clicked = click_image('test_input.png',
+                          region=None,
+                          confidence=0.8,
+                          min_confidence=0.5,
+                          retries=3,
+                          x_reduction=0.2,
+                          y_reduction=0.2,
+                          duration_range=(1, 5))
+    last_clicked = driver.execute_script("return window.lastClickedElement;")
+    assert clicked
+    assert last_clicked == 'test-input'

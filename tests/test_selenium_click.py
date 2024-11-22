ddc5896911c4f2f1c6a77173024ac3ab1cf411b5
lauren <reyuqt01@gmail.com>
2024-11-22 18:33:20 -0500

    ``` fix: update click_this_element function parameters in test_selenium_click.py (main) ```

--- tests/test_selenium_click.py
+++ tests/test_selenium_click.py
@@ -5,7 +5,7 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import os
-from click_helper.selenium_click import get_element, click_image, click_this_element
+from click_helper.selenium_click import get_element, click_this_element, save_element
 
 
 @pytest.fixture(scope="module")
@@ -20,14 +20,24 @@ def test_get_element(driver):
     button = get_element(driver, (By.ID, 'test-button-1'))
     assert button is not None
 
+def test_save_element(driver):
+    selectors_to_test = [(By.ID, 'test-button-1')]
+    for selector in selectors_to_test:
+        element = driver.find_element(*selector)
+        file_path = save_element(element, name='test-button-1')
+        assert 'test-button-1' in file_path
 
 def test_click_this_element(driver):
-    time.sleep(3)
-    clicked = click_this_element(driver, selector=(By.ID, 'test-button-1'),
-                                 confidence=0.8,
-                                 retries=3,
-                                 x_reduction=0.2,
-                                 y_reduction=0.2,
-                                 duration_range=(1, 3),
-                                 )
-    assert clicked
+    selectors_to_test = [(By.ID, 'test-button-1')]
+    for selector in selectors_to_test:
+        clicked = click_this_element(driver, selector=selector,
+                                     confidence=0.8,
+                                     retries=3,
+                                     x_reduction=0.2,
+                                     y_reduction=0.2,
+                                     duration_range=(1, 3),
+                                     )
+        last_clicked = driver.execute_script("return window.lastClickedElement;")
+
+        assert clicked
+        assert last_clicked == selector[1]

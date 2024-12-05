982078f8b553f0ca8b28223d4019eaa4596e13b0
lauren <reyuqt01@gmail.com>
2024-12-05 16:36:39 -0500

    ```plaintext ✨ feat(easing): add cubic easing functions and import math module
    - Added ease_in_out_cubic function for cubic easing.
    - Included math module for future calculations.
    ```

--- webdriver_click_functions/easing.py
+++ webdriver_click_functions/easing.py
@@ -1,3 +1,5 @@
+import math
+
 import pyautogui
 
 MOUSE_MOVEMENTS = [
@@ -15,21 +17,21 @@ def linear(t: float) -> float:
     """Linear easing function."""
     return t
 
-
+""" CUBIC """
 def ease_in_cubic(t: float) -> float:
     """Ease-in cubic easing function."""
     return t ** 3
-
 
 def ease_out_cubic(t: float) -> float:
     """Ease-out cubic easing function."""
     t -= 1
     return t ** 3 + 1
-
-
-def ease_in_out_sine(t: float) -> float:
-    """Ease-in-out sine function."""
-    return -(math.cos(math.pi * t) - 1) / 2
+def ease_in_out_cubic(t: float) -> float:
+    """Ease-in-out cubic easing function."""
+    if t < 0.5:
+        return 4 * t ** 3
+    t -= 1
+    return (2 * t) ** 3 + 1
 
 
 def ease_in_quad(t: float) -> float:

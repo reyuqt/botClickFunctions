a0d64875ee61326f1e774b1cd0d2b75b57626ccb
lauren <reyuqt01@gmail.com>
2024-12-05 16:27:46 -0500

    feat(mouse): add linear easing function to click_with_bezier tests
    - Updated test_click_with_bezier_linear to use linear easing function.
    - Renamed test_click_with_bezier_linear to test_click_with_bezier_ease.

--- tests/test_mouse.py
+++ tests/test_mouse.py
@@ -1,6 +1,6 @@
 import time
 
-from webdriver_click_functions.mouse import click_image, click_at_coordinates, click_with_bezier, ease_in_out_sine
+from webdriver_click_functions.mouse import click_image, click_at_coordinates, click_with_bezier, ease_in_out_sine, linear
 import random
 
 def test_click_at_coordinates(driver, button_box, input_box):
@@ -16,17 +16,17 @@ def test_click_at_coordinates(driver, button_box, input_box):
 
 def test_click_with_bezier_linear(driver, button_box, input_box):
     x, y = button_box.center()
-    click_with_bezier(target=(x, y), duration=random.uniform(1,3), steps=random.randint(100,300))
+    click_with_bezier(target=(x, y), duration=random.uniform(1,3), steps=random.randint(100,300), easing_func=linear)
     last_clicked = driver.execute_script("return window.lastClickedElement;")
     assert last_clicked == 'test-button-1'
 
     time.sleep(0.5)
     x, y = input_box.center()
-    click_with_bezier(target=(x, y), duration=random.uniform(1,3), steps=random.randint(100,300))
+    click_with_bezier(target=(x, y), duration=random.uniform(1,3), steps=random.randint(100,300), easing_func=linear)
     last_clicked = driver.execute_script("return window.lastClickedElement;")
     assert last_clicked == 'test-input'
 
-def test_click_with_bezier_linear(driver, button_box, input_box):
+def test_click_with_bezier_ease(driver, button_box, input_box):
     x, y = button_box.center()
     click_with_bezier(target=(x, y), duration=random.uniform(1,3), steps=random.randint(100,300), easing_func=ease_in_out_sine)
     last_clicked = driver.execute_script("return window.lastClickedElement;")

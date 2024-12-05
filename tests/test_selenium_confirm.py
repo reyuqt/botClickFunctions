c7a9b6431c8b24b990a95d21609c7f0fd2b73e4d
lauren <reyuqt01@gmail.com>
2024-12-04 22:17:05 -0500

    ✨ feat(tests): add default click test for confirmation button

---
+++ tests/test_selenium_confirm.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+from webdriver_click_functions.selenium.click import get_element, click_this_element, save_element, \
+    click_inside_this_element
+from webdriver_click_functions.selenium.confirm import Clicked
+
+def test_clicked_default(driver):
+    clicked = click_this_element(driver, selector=(By.ID, 'test-button-1'),
+                                 confidence=0.8, perform_test=Clicked.default
+                                 )
+    assert clicked
